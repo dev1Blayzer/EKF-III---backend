455ed996e0908ddf95cdda7e6c9adb5d5470219c
eatkidfriendly <info@eatkidfriendly.com>
2020-11-17 11:56:16 -0600

    fix(): update for seed

--- src/seeds/users/Fcvo4EPVu7d7K0z7A1H0oindJVU2.ts
+++ src/seeds/users/Fcvo4EPVu7d7K0z7A1H0oindJVU2.ts
@@ -5,6 +5,6 @@ import { User } from "../../models/User";
 export default function(db: admin.firestore.Firestore): Partial<User> {
   return {
     email: "bobby@madnesslabs.net",
-    name: "Bobby Johnson"
+    given_name: "Bobby Johnson"
   };
 }
